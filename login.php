c0a491cd81723c05c1bdb1ebdac5d751e962b6ce
WahyuAP <wahyu7977@students.amikom.ac.id>
2016-11-19 14:30:17 +0700

    percobaan pertama

---
+++ login.php
@@ -0,0 +1,21 @@
+         <h2>Log In</h2>
+         <form action="loginproses.php" method="post" class="niceform">
+         
+                <fieldset>
+                    <dl>
+                        <dt><label for="email" >Username:</label></dt>
+                        <dd><input type="text" name="id" id="" size="54" placeholder="Username" /></dd>
+                    </dl>
+                    <dl>
+                        <dt><label for="password" >Password:</label></dt>
+                        <dd><input type="password" name="pass" id="" placeholder="Password" size="54" /></dd>
+                    </dl>
+                    
+                     <dl class="submit">
+                    <input type="submit" name="masuk" id="submit" value="Masuk" />
+                     </dl>
+                    
+                </fieldset>
+                
+         </form>
+        
